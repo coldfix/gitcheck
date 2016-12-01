cbf8f1bbac4ed749bed6d4d011cae1852e2cc346
Thomas Gläßle <t_glaessle@gmx.de>
2016-12-02 00:03:13 +0100

    Use more distinctive status flags

--- gitcheck.py
+++ gitcheck.py
@@ -4,12 +4,71 @@ import os
 import sys
 import subprocess
 
-command = ['git', 'diff', '--quiet', '--ignore-submodules=dirty']
-DEVNULL = open(os.devnull, 'r+')
+import operator
 
-def is_clean(folder):
-    return 0 == subprocess.call(command, cwd=folder,
-                                stdout=DEVNULL, stderr=DEVNULL)
+
+def alias(name):
+    return property(operator.attrgetter(name))
+
+
+class GitFlags:
+
+    def __init__(self, flags):
+        flags.discard(' ')
+        self.flags    = flags
+        self.clean    = not bool(flags)
+        self.modified = 'M' in flags
+        self.added    = 'A' in flags
+        self.deleted  = 'D' in flags
+        self.renamed  = 'R' in flags
+        self.copied   = 'C' in flags
+        self.unmerged = 'U' in flags
+
+
+class GitStatus:
+
+    GIT_CLEAN = ['git', 'diff', '--quiet', '--ignore-submodules=dirty']
+    GIT_STATUS = ['git', 'status', '--porcelain', '-b']
+    DEVNULL = open(os.devnull, 'r+')
+
+    untracked = False
+    clean = True
+    branch = None
+
+    def __init__(self, folder):
+        clean = subprocess.call(self.GIT_CLEAN, cwd=folder,
+                                stdout=self.DEVNULL, stderr=self.DEVNULL)
+        status = subprocess.check_output(self.GIT_STATUS, cwd=folder)
+
+        self.clean = clean == 0
+
+        index_flags = set()
+        workdir_flags = set()
+        for l in status.splitlines():
+            if l.startswith(b'## '):
+                if l.endswith(b'ahead'):
+                    self.branch = 'ahead'
+                elif l.endswith(b'behind'):
+                    self.branch = 'behind'
+                elif l.endswith(b'diverged'):
+                    self.branch = 'diverged'
+            elif l.startswith(b'?? '):
+                self.untracked = True
+            else:
+                i, w = l[:2].decode('utf-8')
+                index_flags.add(i)
+                workdir_flags.add(w)
+
+        self.workdir_status = GitFlags(workdir_flags)
+        self.index_status = GitFlags(index_flags)
+
+    def code(self):
+        return ''.join([
+            ' ' if self.workdir_status.clean else 'W',
+            ' ' if self.index_status.clean else 'I',
+            'T' if self.untracked else ' ',
+            'B' if self.branch else ' ',
+        ])
 
 
 def collect_git_repositories(folder):
@@ -30,9 +89,10 @@ def main(folder=None):
     folder = os.path.realpath(folder)
 
     for folder in collect_git_repositories(folder):
-        if is_clean(folder):
+        status = GitStatus(folder)
+        if status.clean:
             continue
-        print(folder)
+        print(status.code(), folder)
 
 
 if __name__ == '__main__':

1a15c4c3b2c4dd8ca93f360344c2b3e4e1fc25ca
Thomas Gläßle <t_glaessle@gmx.de>
2016-12-02 01:32:14 +0100

    Fix sync_status

--- gitcheck.py
+++ gitcheck.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import subprocess
+import re
 
 import operator
 
@@ -29,24 +30,27 @@ class GitStatus:
 
     GIT_STATUS = ['git', 'status', '--porcelain', '-b']
     DEVNULL = open(os.devnull, 'r+')
+    SYNC_PART = re.compile(r'\[([^\[\]]*)\]$')
 
+    gone = False
     untracked = False
-    clean = True
-    branch = None
 
     def __init__(self, folder):
         status = subprocess.check_output(self.GIT_STATUS, cwd=folder)
+        self.sync_status = {}
 
         index_flags = set()
         workdir_flags = set()
         for l in status.decode('utf-8').splitlines():
             if l.startswith('## '):
-                if l.endswith('ahead'):
-                    self.branch = 'ahead'
-                elif l.endswith('behind'):
-                    self.branch = 'behind'
-                elif l.endswith('diverged'):
-                    self.branch = 'diverged'
+                sync_status = self.SYNC_PART.search(l)
+                if sync_status is not None:
+                    for part in sync_status.group(1).split(','):
+                        if part.strip() == 'gone':
+                            self.gone = True
+                        else:
+                            key, val = part.split()
+                            self.sync_status[key] = int(val)
             elif l.startswith('?? '):
                 self.untracked = True
             else:
@@ -57,16 +61,23 @@ class GitStatus:
         self.workdir_status = GitFlags(workdir_flags)
         self.index_status = GitFlags(index_flags)
 
+        self.ahead = self.sync_status.get('ahead', 0)
+        self.behind = self.sync_status.get('behind', 0)
+
     @property
     def clean(self):
         return self.workdir_status.clean and self.index_status.clean
+
+    @property
+    def synced(self):
+        return self.ahead == 0 and self.behind == 0 and not self.gone
 
     def code(self):
         return ''.join([
             ' ' if self.workdir_status.clean else 'W',
             ' ' if self.index_status.clean else 'I',
             'T' if self.untracked else ' ',
-            'B' if self.branch else ' ',
+            ' ' if self.synced else 'S',
         ])
 
 

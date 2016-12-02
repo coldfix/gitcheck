55499448a2c9efc892ba0dc7816712a7ac0ecc78
Thomas Gläßle <t_glaessle@gmx.de>
2016-12-02 12:23:31 +0100

    Move parsing of sync status to its own class

--- gitcheck.py
+++ gitcheck.py
@@ -22,6 +22,34 @@ GIT_STATUS = ['git', 'status', '--porcelain', '--branch']
 # Get sync status for each branch
 SYNC_PART = re.compile(r'\[([^\[\]]*)\]$')
 
+# Related commands:
+# - List remotes for all branches:
+#       git config --get-regexp ^branch\..*\.remote$
+# - Get remote for a specific branch:
+#       git rev-parse --symbolic-full-name --abbrev-ref master@{u}
+
+
+class GitSyncStatus:
+
+    gone = False
+    ahead = 0
+    behind = 0
+
+    def __init__(self, status_line):
+        sync_status = SYNC_PART.search(status_line)
+        if sync_status is None:
+            return
+        for part in sync_status.group(1).split(','):
+            if part.strip() == 'gone':
+                self.gone = True
+                continue
+            key, val = part.split()
+            setattr(self, key, int(val))
+
+    @property
+    def synced(self):
+        return self.ahead == 0 and self.behind == 0 and not self.gone
+
 
 class GitFlags:
 
@@ -40,25 +68,16 @@ class GitFlags:
 
 class GitStatus:
 
-    gone = False
+    sync_status = None
     untracked = False
 
     def __init__(self, folder):
         status = subprocess.check_output(GIT_STATUS, cwd=folder)
-        self.sync_status = {}
-
         index_flags = set()
         workdir_flags = set()
         for l in status.decode('utf-8').splitlines():
             if l.startswith('## '):
-                sync_status = SYNC_PART.search(l)
-                if sync_status is not None:
-                    for part in sync_status.group(1).split(','):
-                        if part.strip() == 'gone':
-                            self.gone = True
-                        else:
-                            key, val = part.split()
-                            self.sync_status[key] = int(val)
+                self.sync_status = GitSyncStatus(l)
             elif l.startswith('?? '):
                 self.untracked = True
             else:
@@ -69,9 +88,6 @@ class GitStatus:
         self.workdir_status = GitFlags(workdir_flags)
         self.index_status = GitFlags(index_flags)
 
-        self.ahead = self.sync_status.get('ahead', 0)
-        self.behind = self.sync_status.get('behind', 0)
-
     @property
     def clean(self):
         return (self.workdir_status.clean and
@@ -80,7 +96,7 @@ class GitStatus:
 
     @property
     def synced(self):
-        return self.ahead == 0 and self.behind == 0 and not self.gone
+        return self.sync_status.synced
 
     def code(self):
         return ''.join([
